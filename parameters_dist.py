75ab3e0095d8d6f0de5214458162e7fb60724e6c
argenisfd <argenisfd@gmail.com>
2016-08-01 16:15:33 -0400

    change parameters file name

---
+++ parameters_dist.py
@@ -0,0 +1,20 @@
+arguments={
+           "dropbox":{
+                      "token":"--dropbox application token--",
+                      "sql_backup_folder":"/sql_backup/"
+                      },
+           "databases":[
+                        { 
+                         "user":"",
+                         "password":"",
+                         "name":"database-1"
+                         },
+                        { 
+                         "user":"",
+                         "password":"",
+                         "name":"database-1"
+                         }
+                        ],
+           "directory_backup":[],
+           
+           }
